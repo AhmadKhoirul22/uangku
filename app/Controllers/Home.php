e8e1da5c01d2c0ff61247ab2aa25560fc22d6c8b
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-06-11 13:11:20 +0700

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,9 +6,18 @@ use App\Models\PengeluaranModel;
 class Home extends BaseController
 {
     public function index(){
-        
+        $pengeluaranModel = new PengeluaranModel();
+        $total_today = $pengeluaranModel->dana_keluar_today();
+        $total_month = $pengeluaranModel->dana_keluar_month();
+        $pengeluaran_harian = $pengeluaranModel->getPengeluaranHarian();
+        $pengeluaran_bulanan = $pengeluaranModel->getPengeluaranBulanan();
+
         $data = [
-            'title' => 'Dashboard'
+            'title' => 'Dashboard',
+            'total_today' => $total_today,
+            'total_month' => $total_month,
+            'pengeluaran_harian' => $pengeluaran_harian,
+            'pengeluaran_bulanan' => $pengeluaran_bulanan,
         ];
         return view('home',$data);
     }
