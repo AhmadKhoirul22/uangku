05b3635b5e5cd44e4d84686ebc27f9e960010f14
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-06-13 13:37:04 +0700

    Update pemasukan.php

--- app/Views/pemasukan.php
+++ app/Views/pemasukan.php
@@ -1,61 +1,58 @@
 <?= $this->extend('template') ?>
 <?= $this->section('content') ?>
 <div class="container">
-    <div class="float-end">
-        <button type="button" data-bs-toggle="modal" data-bs-target="#modalAdd" class="btn btn-outline-primary" >Tambah</button>
+    <div class="text-end">
+        <button type="button" id="" class="btn btn-outline-primary">Tambah</button>
+    </div>
+    <div class="card">
+        <div class="card-body">
+            <table class="table table-striped" id="myTable" width="100%">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Keterangan</th>
+                        <th>Nominal</th>
+                        <th>Tanggal</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
     </div>
 </div>
+
+
 <!-- Modal -->
 <div class="modal fade" id="modalAdd" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title" id="modalCenterTitle">Tambah Pemasukan</h5>
-        <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-        ></button>
-        </div>
-        <div class="modal-body">
-            <form action="" method="post" >
-                <div class="mb-3">
-                    <label class="form-label" for="basic-default-fullname">Keterangan</label>
-                    <input type="text" class="form-control" id="basic-default-fullname" placeholder="John Doe">
-                </div>
-                <div class="mb-3">
-                    <label class="form-label" for="basic-default-company">Nominal</label>
-                    <input type="text" class="form-control" id="basic-default-company" placeholder="ACME Inc.">
-                </div>
-                <div class="mb-3">
-                    <label class="form-label" for="basic-default-email">Tanggal</label>
-                    <input type="date" class="form-control" id="basic-default-company" placeholder="ACME Inc.">
-                </div>
-        </div>
-        <div class="modal-footer">
-        <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-            Close
-        </button>
-        <button type="submit" class="btn btn-primary">Save changes</button>
-        </div>
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalCenterTitle">Tambah Pemasukan</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="" method="post">
+                    <div class="mb-3">
+                        <label class="form-label" for="basic-default-fullname">Keterangan</label>
+                        <input type="text" class="form-control" id="basic-default-fullname" placeholder="John Doe">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label" for="basic-default-company">Nominal</label>
+                        <input type="text" class="form-control" id="basic-default-company" placeholder="ACME Inc.">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label" for="basic-default-email">Tanggal</label>
+                        <input type="date" class="form-control" id="basic-default-company" placeholder="ACME Inc.">
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+                    Close
+                </button>
+                <button type="submit" class="btn btn-primary">Save changes</button>
+            </div>
             </form>
-    </div>
-    </div>
-</div>
-<div class="card">
-    <div class="card-body">
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Keterangan</th>
-                    <th>Nominal</th>
-                    <th>Tanggal</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-        </table>
+        </div>
     </div>
 </div>
+
 <?= $this->endSection() ?>
